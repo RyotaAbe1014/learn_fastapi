05c033190b3f0467cdbbb5a1242f2ad45d73e97c
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-09-15 22:48:49 +0900

    add: pathパラメーター

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/hello")
 async def index():
     return {"message": "hello world"}
+
+
+@app.get("/hello2")
+async def index():
+    return {"message": "hello world2"}
+
+
+@app.get("/countries/{country_name}")
+async def country(country_name: str):
+    return {"country_name": country_name}
+
+
+@app.get("/countries/japan")
+async def country_japan():
+    return {"message": "japan"}
