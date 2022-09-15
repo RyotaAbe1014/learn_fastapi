ad04c25f8c0a29b62e69ac9297b607ac804f06f9
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-09-15 23:00:31 +0900

    add: パスパラメーター

--- main.py
+++ main.py
@@ -21,3 +21,19 @@ async def country(country_name: str):
 @app.get("/countries/japan")
 async def country_japan():
     return {"message": "japan"}
+
+
+@app.get("/countries/")
+async def country(country_name: str = "japan", country_number: int = 1):
+    return {
+        "country_name": country_name,
+        "country_number": country_number,
+    }
+
+
+@app.get("/countries/{country_name}")
+async def country(country_name: str = "japan", city_name: str = "tokyo"):
+    return {
+        "country_name": country_name,
+        "country_number": city_name,
+    }
