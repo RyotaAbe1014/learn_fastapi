cc339f23db6b8f780b4f93055f9718954caa54e5
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-10-07 00:28:15 +0900

    add: APIテスト 会議室一覧作成

--- app_test/app.py
+++ app_test/app.py
@@ -3,22 +3,43 @@ import random
 import requests
 import json
 
-st.sidebar.selectbox('めにゅー', ['users', 'rooms'])
-st.title("ユーザーAPIテスト")
+page = st.sidebar.selectbox('めにゅー', ['users', 'rooms'])
 
-with st.form(key='users'):
-    user_id: int = random.randint(0, 10)
-    username: str = st.text_area('ユーザー名', max_chars=12)
-    data = {
-        'user_id': user_id,
-        'username': username
-    }
-    submit_button = st.form_submit_button(label='送信')
+if page == 'users':
+    st.title("ユーザーAPIテスト")
+    with st.form(key='users'):
+        user_id: int = random.randint(0, 10)
+        username: str = st.text_area('ユーザー名', max_chars=12)
+        data = {
+            'user_id': user_id,
+            'username': username
+        }
+        submit_button = st.form_submit_button(label='送信')
 
+    if submit_button:
+        st.write('送信データ')
+        st.json(data)
+        url = "http://127.0.0.1:8000/users"
+        res = requests.post(url, data=json.dumps(data))
+        st.json(res.json())
 
-if submit_button:
-    st.write('送信データ')
-    st.json(data)
-    url = "http://127.0.0.1:8000/users"
-    res = requests.post(url, data=json.dumps(data))
-    st.json(res.json())
+elif page == 'rooms':
+    st.title("会議室APIテスト")
+    with st.form(key='room'):
+        room_id: int = random.randint(0, 10)
+        room_name: str = st.text_area('会議室名', max_chars=12)
+        capacity: int = st.number_input('定員', step=1)
+
+        data = {
+            'room_id': room_id,
+            'room_name': room_name,
+            'capacity': capacity
+        }
+        submit_button = st.form_submit_button(label='送信')
+
+    if submit_button:
+        st.write('送信データ')
+        st.json(data)
+        url = "http://127.0.0.1:8000/rooms"
+        res = requests.post(url, data=json.dumps(data))
+        st.json(res.json())
