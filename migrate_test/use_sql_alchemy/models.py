ff7507e70c474e56fc502ec2a507160182b22697
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-12-30 13:23:49 +0900

    add: リレーションのテスト用にTaskモデル作成

--- migrate_test/use_sql_alchemy/models.py
+++ migrate_test/use_sql_alchemy/models.py
@@ -1,12 +1,30 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 # モデルのベースクラス
 Base = declarative_base()
 
 # モデルクラス
+
+
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     age = Column(Integer)
+    tasks = relationship('Task', back_populates='user')
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey(
+        "users.id", onupdate='CASCADE', ondelete='CASCADE'))
+    title = Column(String)
+    user = relationship('User', back_populates='tasks')
+
+
+# back_populatesパラメータを使用することで、双方向のリレーションを張ることができる
+# UserモデルクラスからTaskモデルクラスを参照するtasksリレーションと
+# TaskモデルクラスからTaskモデルクラスを参照するuserリレーションを同時に張っています。
