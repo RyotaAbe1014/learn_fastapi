e2f94c54bdb62786b7bcb5dc4a6c7d30b709f0b6
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-01-08 19:24:39 +0900

    タスク詳細機能を作成

--- task_app/cruds/task.py
+++ task_app/cruds/task.py
@@ -5,3 +5,9 @@ import schemas.task
 
 def get_tasks(db: Session):
     return db.query(Task).all()
+
+
+def get_task(db: Session, id: int):
+    return db.query(Task).filter(Task.id == id).first()
+
+
--- task_app/routers/task.py
+++ task_app/routers/task.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from typing import List
 from schemas.task import Task as TaskSchema
-from cruds.task import get_tasks
+from cruds.task import get_tasks, get_task
 from db import session
 
 router = APIRouter()
@@ -16,6 +16,11 @@ async def list_tasks():
 async def create_task():
     pass
 
+
+@router.get("/tasks/{task_id}", response_model=TaskSchema)
+async def detail_task(id: int):
+    return get_task(session, id)
+    
 
 @router.put("/tasks/{task_id}")
 async def update_task():
