9418018f609e8d43feb24f96d8d5f33b65c09dc3
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-01-09 22:16:52 +0900

    タスク取得機能に戻り値の型を指定

--- task_app/cruds/task.py
+++ task_app/cruds/task.py
@@ -1,13 +1,16 @@
 from sqlalchemy.orm import Session
 from models.task import Task
-import schemas.task
+from schemas.task import TaskCreate
+from typing import List, Optional
 
 
-def get_tasks(db: Session):
+def get_tasks(db: Session) -> Optional[List[Task]]:
     return db.query(Task).all()
 
 
-def get_task(db: Session, id: int):
+def get_task(db: Session, id: int) -> Optional[Task]:
     return db.query(Task).filter(Task.id == id).first()
 
 
+def create_task(db: Session, task_schema: TaskCreate):
+    pass
