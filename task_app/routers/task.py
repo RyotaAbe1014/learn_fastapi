3ca61bc79a4adc6b57796507d329ecc2299ae465
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-01-15 01:51:33 +0900

    test

--- task_app/routers/task.py
+++ task_app/routers/task.py
@@ -4,30 +4,30 @@ import schemas.task as TaskSchema
 import cruds.task as crud
 from db import session
 from settings.custom_route import CustomRoute
-router = APIRouter()
+router = APIRouter(prefix="/tasks", tags=["tasks"])
 router.route_class = CustomRoute
 
 
-@router.get("/tasks", response_model=List[TaskSchema.Task])
+@router.get("/", response_model=List[TaskSchema.Task])
 async def list_tasks():
     return crud.get_tasks(session)
 
 
-@router.post("/tasks", response_model=TaskSchema.Task)
+@router.post("/", response_model=TaskSchema.Task)
 async def create_task(task: TaskSchema.TaskCreate):
     return crud.create_task(session, task)
 
 
-@router.get("/tasks/{task_id}", response_model=TaskSchema.Task)
+@router.get("/{task_id}", response_model=TaskSchema.Task)
 async def detail_task(task_id: int):
     return crud.get_task(session, task_id)
 
 
-@router.put("/tasks/{task_id}", response_model=TaskSchema.Task)
+@router.put("/{task_id}", response_model=TaskSchema.Task)
 async def update_task(task: TaskSchema.TaskUpdate, task_id: int):
     return crud.update_task(session, task_id, task)
 
 
-@router.delete("/tasks/{task_id}",  response_model=TaskSchema.Task)
+@router.delete("/{task_id}",  response_model=TaskSchema.Task)
 async def delete_task(task_id: int):
     return crud.delete_task(session, task_id)
