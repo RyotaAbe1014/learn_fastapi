c629b58ac24cc215b8472d1c7600baa388c29c05
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-01-18 23:43:23 +0900

    add: taskスキーマーにカスタムバリデーションを実装

--- task_app/schemas/task.py
+++ task_app/schemas/task.py
@@ -1,10 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 import typing as t
 
 
 class TaskBase(BaseModel):
     title: str
     content: t.Optional[str]
+
+    @validator("title")
+    def check_title_length(cls, value):
+        if len(value) > 30:
+            raise ValueError("titleは30文字以内で入力してください。")
+        return value
+
+    @validator("content")
+    def check_content_length(cls, value):
+        if len(value) > 255:
+            raise ValueError("contentは255文字以内で入力してください。")
+        return value
 
 
 class TaskCreate(TaskBase):
