7a267107c9ae5ad60ec4a12d0ef44d9004cc2894
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-01-19 23:48:25 +0900

    add: リクエストチェックが入る前に独自処理を記述できるように

--- task_app/main.py
+++ task_app/main.py
@@ -1,12 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
 from settings.custom_route import CustomRoute
 from routers import task
+from pydantic import BaseModel
+
 
 app = FastAPI()
 app.router.route_class = CustomRoute
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        content={"message": exc.detail},
+        status_code=exc.status_code
+    )
+
+
+@app.middleware("http")
+async def auth_middleware(request: Request, call_next):
+    """
+    カスタムエラーハンドリング
+    コメントを外すと、認証が必要なエンドポイントにアクセスすると
+    FastAPIよりも先に401が返る
+    """
+    # if not request.headers.get("Authorization"):
+    #     return JSONResponse(content={"message": "Not authorized"}, status_code=401)
+    response = await call_next(request)
+    return response
+
 app.include_router(task.router)
 
 
-@app.get("/")
-async def hello():
-    return {"message": "hello world"}
+"""
+実験用
+"""
+
+
+class Message(BaseModel):
+    message: str
+
+
+@app.post("/")
+async def hello(message: Message):
+    return {"message": message.message}
