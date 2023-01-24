04f5b82f91d2f620afaf39b6c80d4b68c176c73d
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-01-24 23:14:15 +0900

    add: タスク処理全般に認証を追加

--- task_app/routers/task.py
+++ task_app/routers/task.py
@@ -1,32 +1,34 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Depends
 from typing import List
 import schemas.task as TaskSchema
 import cruds.task as crud
 from db import session
 from settings.custom_route import CustomRoute
+from auth.oauth2 import oauth2_scheme
+
 router = APIRouter(prefix="/tasks", tags=["tasks"], route_class=CustomRoute)
 
 
 @router.get("/", response_model=List[TaskSchema.Task])
-async def list_tasks():
+async def list_tasks(token: str = Depends(oauth2_scheme)):
     return crud.get_tasks(session)
 
 
 @router.post("/", response_model=TaskSchema.Task)
-async def create_task(task: TaskSchema.TaskCreate):
+async def create_task(task: TaskSchema.TaskCreate, token: str = Depends(oauth2_scheme)):
     return crud.create_task(session, task)
 
 
 @router.get("/{task_id}", response_model=TaskSchema.Task)
-async def detail_task(task_id: int):
+async def detail_task(task_id: int, token: str = Depends(oauth2_scheme)):
     return crud.get_task(session, task_id)
 
 
 @router.put("/{task_id}", response_model=TaskSchema.Task)
-async def update_task(task: TaskSchema.TaskUpdate, task_id: int):
+async def update_task(task: TaskSchema.TaskUpdate, task_id: int, token: str = Depends(oauth2_scheme)):
     return crud.update_task(session, task_id, task)
 
 
 @router.delete("/{task_id}",  response_model=TaskSchema.Task)
-async def delete_task(task_id: int = Path(..., description="タスクID")):
+async def delete_task(task_id: int = Path(..., description="タスクID"), token: str = Depends(oauth2_scheme)):
     return crud.delete_task(session, task_id)
