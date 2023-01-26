dccaa2aca01f0d3b36339dfe12e3c106a740cec7
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-01-26 22:58:18 +0900

    fix: usernameがユニークになっていなかったのでユニークに変更

--- task_app/models/user.py
+++ task_app/models/user.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(30))
+    username = Column(String(30), unique=True)
     email = Column(String(30), unique=True)
     password = Column(String(255))
     is_active = Column(Boolean, default=True)
