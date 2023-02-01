f88c2e991315565c703b075680cf66cfc4a568fe
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-02-01 18:06:09 +0900

    add: さまざまなリクエストの判定のやり方

--- path_description.py
+++ path_description.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI, Depends, Path, Query
+from fastapi import FastAPI, Depends, Path, Query, Body
 from pydantic import BaseModel, Field
 app = FastAPI()
 
-class Item(BaseModel):
-    item_id : int = Field(Path(description="aaaa"))
-    item_name: str = Field(Query(description="bbbb"))
 
+
+
+
+class Item(BaseModel):
+    item_id : int = Field(Path(None,description="aaaa"))
+    item_name: str = Field(Query(None, description="bbbb"))
+    item_name2: str = Field(Body(None, description="ccc"))
 
 
 
