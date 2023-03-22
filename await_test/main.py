2a51b95b7bda7fb62f5c877f86d492501bad0dde
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-03-23 00:08:43 +0900

    add: 非同期サンプル

--- await_test/main.py
+++ await_test/main.py
@@ -1,17 +1,27 @@
-import requests
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import BackgroundTasks, FastAPI
+import time
+
 
 app = FastAPI()
 
+# async/awaitを使用する必要があるケースは
+# メール送信処理など、時間がかかる処理を行う場合や重いI/O処理を行う場合(SQLAlchemyでは非同期に対応しているのでcreate_async_engine, AsyncSessionを使う場合tukaubaai)
+# またはフレームワークの機能を使用する場合や、外部のAPIを叩く場合にawaitを使用する必要がある場合など
 
-def run_task():
-    # 非同期処理
-    pass
+# 例えば、メール送信処理を行う場合、メール送信処理が終わるまでレスポンスを返すことができない
+# そのため、メール送信処理をキューに追加し、レスポンスを返すことで、メール送信処理を非同期で行うことができる
+# このような場合には、fastAPIのBackgroundTasksを使用する
+# BackgroundTasksを使用する場合には、async/awaitを使用する必要がある
 
+def send_email(email: str, message: str):
+    # メール送信処理
+    # 10秒待機
+    time.sleep(10)
+    print("メール送信処理")
 
-
-
-@app.post("/tasks")
-async def create_task(background_tasks: BackgroundTasks):
-    background_tasks.add_task(run_task) # 非同期処理
-    return {"message": "Task created"}
+@app.post("/send-email")
+async def send_email_view(email: str, message: str, background_tasks: BackgroundTasks):
+    # メール送信処理をキューに追加
+    background_tasks.add_task(send_email, email, message)
+    # レスポンスを返す
+    return {"message": "Email has been sent"}
